d393fc484461645f7a7e3782aac1cd9588131f1c
Michael O'Connell <michaeljoconnell@ufl.edu>
2019-06-14 17:51:51 -0400

    Code runs faster, can be run multiple times with no errors, better UX

--- Batch_Image_Processing/Image_Scripts/Rename.py
+++ Batch_Image_Processing/Image_Scripts/Rename.py
@@ -1,30 +1,38 @@
 import os
 
 def main():
+	ext = ".CR2"
+	print("This program will help you rename " + ext + " files in a directory")
 	path = input("Please type the path of the directory: ")
+	
+	# Valid paths must end in a separator, this checks and adds one
+	# Every os fs accepts "/", yup even Windows, cool right!?
 	if not path.endswith("/") or not path.endswith("\\"):
-		path + "/"
+		path += "/" 
 	
 	
 	for filename in os.listdir(path):
-		fn = filename
-		print(fn)
+		#fn represents the new filename
+		fn = filename 
 		
-		fn = fn.replace(".CR2","")
-		print(fn)
-		#(2) becomes _V
-		fn = fn.replace(" (2)","_V")
-		print(fn)
-		
-		#Spaces to Underscore
-		fn = fn.replace(" ", "_")
-		print(fn)
-		# if not _V or _L become _D
-		if (fn.find("_V") == -1 and fn.find("_L") == -1):
-			fn += "_D"
-		#Write to Filename
-		os.rename(path+filename, path+fn+".CR2")
-		print(fn)
+		# Only rename cr2 images
+		if (fn.find(ext) != -1):
+			# remove extension for now
+			fn = fn.replace(ext,"")
+			
+			#" (2)" becomes _V (Use the space to avoid double underscore
+			fn = fn.replace(" (2)","_V")
+			
+			# Spaces to Underscore
+			fn = fn.replace(" ", "_")
+			
+			# if not _V or _L or _D become _D
+			if (fn.find("_V") == -1 and fn.find("_L") == -1 and fn.find("_D") == -1):
+				fn += "_D"
+				
+			#Rename if the filename has changed (saves time and processing power)
+			if(filename != fn + ext):
+				os.rename(path + filename, path + fn +ext)
 		
 #Driver code 
 if __name__ == '__main__': 
