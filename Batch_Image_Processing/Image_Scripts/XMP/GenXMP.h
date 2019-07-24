f47fe5b3dc3b6c8c8d57cc599816bad56b12ac30
NotoriousENG <michaeljoconnell@ufl.edu>
2019-07-24 13:37:31 -0400

    reorg pt2

--- Batch_Image_Processing/Image_Scripts/XMP/GenXMP.h
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-#include<string>
-
-
-void Generate(string filename);
-void ListFiles(string path);
--- Batch_Image_Processing/Image_Scripts/mydmtx.py
+++
@@ -1,7 +0,0 @@
-from pylibdmtx.pylibdmtx import decode
-from PIL import Image
-
-print('Start')
-var = decode(Image.open('default.jpg'))
-print(var.data)
-print('End')
