34e5c66f11d0699112749815241dfba83dfbb042
Aaron Leopold <aaronleopold1221@gmail.com>
2020-02-11 09:47:32 -0600

    added the file organization to downscale script

--- scripts/Rescale/rescale.py
+++ scripts/Rescale/rescale.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from PIL import Image
 
 
@@ -53,15 +54,24 @@ def rescurse_prompt():
         recurse = input("\nInvalid input. Choose 1 of the following: \n [1]Standard (All image files in this directory level) \n [2]Recursive " \
             "(All image files in this directory level and every level below) \n--> ")
 
-    return recurse
+    if recurse is '1' or recurse is '[1]':
+        return False
+    
+    else: return True
 
 
 def get_subdirs(path):
     subdirs = []
     for item in sorted(os.listdir(path)):
-        if os.path.isdir(item):
+        if os.path.isdir(path + item):
             subdirs.append(item)
+
     return subdirs
+
+
+def moveCR2(path, img):
+    pass
+    shutil.move(path + img, path + 'CR2/' + img)
 
 
 def rescale(path, name, scale):
@@ -75,9 +85,21 @@ def rescale(path, name, scale):
     new_name = name.split('.')[0] + '_downscaled.' + ext
 
     new_image = img.resize(new_size)
-    new_image.save(path + new_name, 'JPEG')
 
     print('{}: Original width and height {} by {}. New width and height {} by {}'.format(path, width, height, new_width, new_height))
+
+    print('Saving downscaled image...')
+    print('Moving downscaled image {} to LO-RES folder...'.format(new_name))
+
+    # save downscaled to low-res folder
+    new_image.save(path + 'LOW-RES/' + new_name, 'JPEG')
+
+    print('Moving original {} to HI-RES folder'.format(name))
+
+    # move original jpg to hi-res folder
+    shutil.move(path + name, path + 'HI-RES/' + name)
+
+    
 
 
 def recursive_run(path, scale):
@@ -87,8 +109,25 @@ def recursive_run(path, scale):
 
 
 def run(path, scale):
+    print('Working in: {}'.format(path))
+    # create the CR2, HIRES, LOWRES folders at this pathway. 
+    if not os.path.exists(os.path.join(path, 'CR2')):
+        os.mkdir(os.path.join(path, 'CR2'))
+
+    if not os.path.exists(os.path.join(path, 'HI-RES')):
+        os.mkdir(os.path.join(path, 'HI-RES'))
+
+    if not os.path.exists(os.path.join(path, 'LOW-RES')):
+        os.mkdir(os.path.join(path, 'LOW-RES'))
+
+
     for img in sorted(os.listdir(path)):
-        rescale(path, img, scale)
+        if img.lower().endswith('.cr2'):
+            moveCR2(path, img)
+        elif img.lower().endswith('.jpg') or img.lower().endswith('.jpeg'):    
+            rescale(path, img, scale)
+        else:
+            print('invalid file type. skipping...')
 
 
 def main():
