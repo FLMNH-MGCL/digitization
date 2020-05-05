d41f2bfb090a65d727f4234ca3879cda1f9e30af
Aaron Leopold <aaronleopold1221@gmail.com>
2020-05-05 13:44:45 -0400

    added filtering options for wls

--- scripts/WLS/wls.py
+++ scripts/WLS/wls.py
@@ -2,6 +2,22 @@
 import os
 import datetime
 import sys
+# https://stackoverflow.com/questions/1394956/how-to-do-hit-any-key-in-python
+try:
+    # Win32
+    from msvcrt import getch
+except ImportError:
+    # UNIX
+    def getch():
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+        print("Press any key to exit...")
+        try:
+            tty.setraw(fd)
+            return sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old)
 
 valid_files = ['JPG', 'jpg', 'CR2', 'cr2', 'PNG', 'png']
 
@@ -12,6 +28,10 @@ class WLS:
     self.target_directory = os.getcwd() + '/'
     self.method = method
     self.filter = None
+
+  
+  def set_filter(self, filter):
+    self.filter = filter
 
   
   def generate_log_arr(self):
@@ -85,7 +105,10 @@ class WLS:
     for file in sorted(os.listdir(path)):
         if os.path.isfile(path + file):
           if (len(file.split('.')) > 1) and (file.split('.')[1] in valid_files):
-            files.append(file)
+            if self.filter is None:
+              files.append(file)
+            elif self.filter in file:
+              files.append(file)
     return files
 
 
@@ -100,7 +123,7 @@ class WLS:
     
     return new_name
 
-  # FIXME
+
   def run_views(self, path):
     files = self.get_files(path)
     dirs = self.get_folders(path)
@@ -113,7 +136,7 @@ class WLS:
     dir_map["PATH"] = path
     for file in files:
       mapped_name = self.parse_name(file)
-      print("Current file: {}\nParsed name: {}".format(file, mapped_name))
+      # print("Current file: {}\nParsed name: {}".format(file, mapped_name))
       if mapped_name in dir_map:
         dir_map[mapped_name].append(file)
       else:
@@ -128,9 +151,12 @@ class WLS:
     dirs = self.get_folders(path)
     dir_arr = []
 
+
     for dir in dirs:
       self.run_unique(path + dir + "/")
     dir_arr.append(path)
+
+    print("Currently working in: {}\n".format(path))
     for file in files:
       mapped_name = self.parse_name(file)
       # check if existing already for dir
@@ -147,6 +173,7 @@ class WLS:
     for dir in dirs:
         self.run(path + dir + '/')
     self.data.append(path + '\n')
+    print("Currently working in: {}\n".format(path))
     for file in files:
         self.data.append(file + '\n')
     self.data.append('\n')
@@ -172,46 +199,89 @@ def help():
   print(help_str)
   # input("Press enter to exit...")
 
+def extract_filter(argument_list, arg_len):
+  if arg_len < 3:
+    return "OK", None
+  
+  elif arg_len == 3:
+    if argument_list[1] in ["--filter", "-f"]:
+      return "OK", argument_list[2]
+    else:
+      # invalid usage here
+      # if isnt filter at pos 1, then arg len is invalid usage
+      return "BAD", None
+
+  elif arg_len == 4:
+    if argument_list[2] in ["--filter", "-f"]:
+      return "OK", argument_list[3]
+    else:
+      return "BAD", None
+  
+  else:
+    return "BAD", None
+
 def main():
   argument_list = sys.argv
-  if len(argument_list) == 1:
+  arg_len = len(argument_list)
+  filter = extract_filter(argument_list, arg_len)
+
+  if filter[0] == "BAD":
+    print(
+      "Incorrect usage. Please run with the help flag for more usage information:\n"
+      "   wls.py --help\n"
+    )
+  else:
+    filter = filter[1]
+
+  if arg_len == 1:
     # run normal wls
     program = WLS()
+    program.set_filter(None)
     program.run(program.target_directory)
     program.write_out()
 
-  else:
+  elif arg_len > 1 and arg_len < 5:
     # parse argument
     target_program = argument_list[1]
 
     if target_program in ["--help", "-h"]:
       help()
-      input("Press enter twice to exit...")
+      getch()
 
     elif target_program in ["--unique-only", "-u"]:
       program = WLS("unique")
+      program.set_filter(filter)
       program.run_unique(program.target_directory)
       program.write_out()
 
     elif target_program in ["--separate-views", "-sv"]:
       program = WLS("views")
+      program.set_filter(filter)
       program.run_views(program.target_directory)
       program.write_out()
 
+    elif target_program in ["--filter", "-f"]:
+      # default wls run with filter
+      if arg_len == 3:
+        filter = argument_list[2]
+        program = WLS()
+        program.set_filter(filter)
+        program.run(program.target_directory)
+        program.write_out()
+      else:
+        # incorrect argument ordering (filter listed first)
+        print(
+          "Incorrect argument ordering. Please run with the help flag for more usage information:\n"
+          "   wls.py --help\n"
+        )
+
+
     else:
       # unknown option
-      pass
+      print("Unknown usage.")
 
     # input("Press enter to exit...")
   
 
 if __name__ == "__main__":
     main()
-
-
-    #   elif len(argument_list) > 2:
-    # # suggest running help command
-    # print(
-    #   "Too many arguments. Please run with the help flag for more usage information:\n"
-    #   "   wls.py --help\n"
-    # )
